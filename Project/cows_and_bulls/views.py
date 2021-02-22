b287310fa9fd203ead3bf60a9dfacb378f15f997
A-miin <zulaykaisaeva@gmail.com>
2021-02-22 16:03:09 +0600

    Add def set_numbers for get new random list

--- Project/cows_and_bulls/views.py
+++ Project/cows_and_bulls/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from random import randint
+# Create your views here.
+numbers=[]
+def set_numbers():
+    global numbers
+    numbers = []
+    while True:
+        a = randint(1,10)
+        if a not in numbers:
+            numbers.append(a)
+        if len(numbers)==4:
+            break
 
 def home(request):
+    set_numbers()
+    print(numbers)
     return render(request, 'home.html')
 
 def game(request):
